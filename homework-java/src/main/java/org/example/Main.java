4f167af1c754db50508c0f7646d44402089a9e6a
xiaozhounandu <2459604063@qq.com>
2022-09-06 18:30:57 +0800

    second

---
+++ homework-java/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @author wei zhijie
+ * @version 1.0
+ * @date ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello world!");
+        System.out.println("java");
+    }
+}
---
+++ javaweb/web/xiaozhounandu/src/main/webapp/WEB-INF/success.jsp
@@ -0,0 +1,12 @@
+
+<form action="${pageContext.request.contextPath}/login" method="get">
+    <input type="text" name="username"><br>
+    <input type="password" name="password"><br>
+    <input type="submit">
+
+
+
+
+
+
+</form>
